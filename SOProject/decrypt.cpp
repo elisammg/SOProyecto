8d0549310b1f28a3347957b8c44a9a4355494d1c
Kevin Champney <kevinchampney@hotmail.com>
2019-09-28 18:20:05 -0600

    Add files

---
+++ SOProject/decrypt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+   std::ifstream file("encrypted_message.txt");
+   if(!file)return 0;
+   std::string line;
+
+   while (std::getline(file, line, '\0')){
+      for(char ascii : line){
+         std::cout<<ascii;
+      }
+   }
+   printf("\n");
+   return 0;
+}
---
+++ SOProject/encrypt.cpp
@@ -0,0 +1,70 @@
+/*
+Programa de encripción con algoritmo DES utilizando variables mutex.
+Alumnos:
+    Kevin Champney
+    Elisa Monzón
+*/
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+/* Segmento de código para obtener mensaje por teclado con getchar() */
+/* Código tomado de Stack Overflow */
+void strInput(char str[], int nchars) {
+    int i = 0;
+    int ch;
+    while((ch = getchar()) != '\n' && ch != EOF ) {
+        if (i < nchars) {
+        str[i++] = ch;
+        }
+    }
+    str[i] = '\0';
+}
+
+int chPrompt(int nchars) {
+    printf("How many chars do you need to input? > ");
+    if (scanf("%i", &nchars) != 1) {
+        printf("Unable to read #\n"); 
+        exit(-1);
+    }
+
+    // Consume remaining text in the line
+    int ch;
+    while((ch = getchar()) != '\n' && ch != EOF );
+        
+    return nchars;
+}
+/* Fin de segmento de código para obtener mensaje por teclado con getchar() */
+
+/* Segmento de código para crear un archivo .txt con el mensaje encriptado para ser decriptado más adelante. */
+int writeFile (char str[]) 
+{
+  ofstream myfile;
+  myfile.open ("encrypted_message.txt");
+  myfile << str;
+  myfile.close();
+  return 0;
+}
+/* Fin de segmento de código para creación de archivo .txt */ 
+
+int main(void) {
+    int nchars = chPrompt(nchars);
+    char str[nchars + 1];  // + 1
+    strInput(str, nchars);
+
+    //Troubleshooting
+    printf("El mensaje escrito tiene %d caracteres\n", (int) strlen(str));
+
+    //LLamado de la función writeFile. 
+    writeFile(str);
+    printf("El archivo ha sido creado con éxito.\n");
+
+    return 0;
+}
+
+
